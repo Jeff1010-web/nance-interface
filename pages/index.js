8387bb50902bb2861a7d6575cbfc6f97b4a8ff41
jeff1010-web <wangping51030@gmail.com>
2022-07-13 23:25:10 +0800

    style: styling with tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,49 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 import ConversionFunnelChart from '../components/ConversionFunnelChart.js'
 import ApprovalChart from '../components/ApprovalRateChart.js'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="px-8">
       <Head>
         <title>JuiceTools</title>
         <meta name="description" content="A bunch of homebrew tools" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">
+      <main className="min-h-screen py-16 flex-1 flex flex-col justify-center items-center">
+        <h1 className="text-7xl font-medium">
           DAO Analytics
         </h1>
         <Link href="https://mirror.xyz/itamarg.eth/Cxs_pTvMQ3-Udj1MYDZW46OZxGnHET1cHQX_tTozckw">
-          <a>Methodology: DAO health</a>
+          <a className="text-3xl hover:underline">Methodology: DAO health</a>
         </Link>
         <br/>
 
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>Member Conversion Funnel &rarr;</h2>
+        <div className="flex items-center justify-center flex-wrap max-w-7xl text-inherit">
+          <div className="m-4 p-6 text-left no-underline rounded-xl border-solid border-slate-100 border-2 max-w-5xl transition-colors hover:border-blue-400 hover:text-blue-400">
+            <h2 className="text-2xl mb-4">Member Conversion Funnel &rarr;</h2>
             <ConversionFunnelChart />
           </div>
 
-          <div className={styles.card}>
-            <h2>Proposal Approval Rate &rarr;</h2>
+          <div className="m-4 p-6 text-left no-underline rounded-xl border-solid border-slate-100 border-2 max-w-5xl transition-colors hover:border-blue-400 hover:text-blue-400">
+            <h2 className="text-2xl mb-4">Proposal Approval Rate &rarr;</h2>
             <ApprovalChart />
           </div>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="flex flex-1 py-16 rounded-t-sm border-solid border-black justify-center items-center">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex justify-center items-center grow"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span className="ml-2 h-4">
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
