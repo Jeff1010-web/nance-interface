51cf1086d65831812d98bc1c192a53827aca8e86
jeff1010-web <wangping51030@gmail.com>
2022-07-18 15:53:40 +0800

    refactor: move down inline components

--- pages/history.js
+++ pages/history.js
@@ -1,7 +1,4 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import Layout from '../components/Layout.js'
-import TimelineItem from '../components/TimelineItem.js'
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 
@@ -67,5 +64,34 @@ export default function History() {
         </VerticalTimeline>
       </div>
     </Layout>
+  )
+}
+
+function TimelineItem({ date, title, text, imgSrc, linkText, link, iconSrc }) {
+  return (
+    <VerticalTimelineElement
+      className="vertical-timeline-element--work"
+      date={date}
+      contentStyle={{ background: 'rgb(235, 235, 235)', color: 'rgb(21, 28, 59)' }}
+      contentArrowStyle={{ borderRight: '7px solid  rgb(235, 235, 235)' }}
+      iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+      icon={<img className="rounded-full" src={iconSrc || "https://juicebox.money/assets/banana-ol.png"} alt="Vercel Logo" />}
+    >
+      <h2 className="vertical-timeline-element-title capitalize">
+        {title}
+      </h2>
+      <p>
+        {text}
+      </p>
+      <div className="rounded-lg overflow-hidden my-2">
+        <img src={imgSrc} alt={imgSrc} />
+      </div>
+      <div className="pt-4 pb-2">
+        <a className="bg-amber-200 hover:bg-opacity-60 transition rounded-md px-3 py-2 text-xs"
+          href={link} target="_blank" rel="noreferrer">
+          {linkText}
+        </a>
+      </div>
+    </VerticalTimelineElement>
   )
 }
--- pages/metric.js
+++ pages/metric.js
@@ -1,9 +1,14 @@
 import Link from 'next/link'
-
-import ConversionFunnelChart from '../components/ConversionFunnelChart.js'
-import ApprovalChart from '../components/ApprovalRateChart.js'
-import VoteParticipationChart from '../components/VoteParticipationChart.js'
 import Layout from '../components/Layout.js'
+
+import {
+  ResponsiveContainer, 
+  ComposedChart, FunnelChart, BarChart,
+  Bar, Line, Funnel,
+  CartesianGrid,
+  XAxis, YAxis, 
+  Tooltip, Legend, LabelList } from 'recharts';
+import { useApprovalGroups, useProposalParticipations } from '../hooks/Proposal';
 
 export default function Metric() {
   return (
@@ -26,7 +31,7 @@ export default function Metric() {
 
             <div className="m-4 p-6 text-left no-underline rounded-xl border-solid border-slate-300 border-2 max-w-xl transition-colors hover:border-blue-400 hover:text-blue-400">
               <h2 className="text-2xl mb-4">Proposal Approval Rate</h2>
-              <ApprovalChart />
+              <ApprovalRateChart />
             </div>
 
             <div className="m-4 p-6 text-left no-underline rounded-xl border-solid border-slate-300 border-2 max-w-xl transition-colors hover:border-blue-400 hover:text-blue-400">
@@ -37,5 +42,116 @@ export default function Metric() {
         </main>
       </div>
     </Layout>
+  )
+}
+
+function ConversionFunnelChart() {
+  const data = [
+    {
+      "value": 16498,
+      "name": "Twitter followers",
+      "fill": "#8884d8"
+    },
+    {
+      "value": 12300,
+      "name": "Discord members",
+      "fill": "#83a6ed"
+    },
+    {
+      "value": 3501,
+      "name": "Token holders",
+      "fill": "#8dd1e1"
+    },
+    {
+      "value": 121,
+      "name": "DAO contributors",
+      "fill": "#82ca9d"
+    },
+    {
+      "value": 25,
+      "name": "Paid contributors",
+      "fill": "#a4de6c"
+    }
+  ]
+
+
+  return (
+    <ResponsiveContainer width="80%" height="80%" minWidth="32rem" minHeight="20rem">
+      <FunnelChart width={730} height={250}>
+        <Tooltip />
+        <Funnel
+          dataKey="value"
+          data={data}
+          isAnimationActive
+        >
+          <LabelList position="right" fill="#000" stroke="none" dataKey="name" />
+        </Funnel>
+      </FunnelChart>
+    </ResponsiveContainer>
+  )
+}
+
+function ApprovalRateChart() {
+  const { loading, data } = useApprovalGroups();
+
+  if (loading) return 'Loading...'
+
+  const renderCustomBarLabel = ({ payload, x, y, width, height, value }) => {
+    return <text x={x + width / 2} y={y} fill="#666" textAnchor="middle" dy={-6}>{`${value}`}</text>;
+  };
+
+  const renderCustomAxisTick = ({ x, y, stroke, payload }) => {
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="start" fill="#666" transform="rotate(10)">
+          {payload.value}
+        </text>
+      </g>
+    );
+  }
+
+  return (
+    <ResponsiveContainer width="80%" height="80%" minWidth="32rem" minHeight="20rem">
+      <ComposedChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Line yAxisId="left" type="monotone" dataKey="approval_rate" stroke="#f4603e" />
+        <Bar yAxisId="right" type="monotone" dataKey="total_proposals" fill="#56569b"
+         label={renderCustomBarLabel} />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="key" interval="preserveStartEnd" tick={renderCustomAxisTick} />
+        <YAxis yAxisId="left" unit="%" />
+        <YAxis yAxisId="right" orientation='right' />
+        <Tooltip />
+        <Legend verticalAlign="top" height={36}/>
+      </ComposedChart>
+    </ResponsiveContainer>
+  )
+}
+
+function VoteParticipationChart() {
+  const { loading, data } = useProposalParticipations();
+
+  if (loading) return 'Loading...'
+
+  const renderCustomAxisTick = ({ x, y, stroke, payload }) => {
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="start" fill="#666" transform="rotate(10)">
+          {payload.value}
+        </text>
+      </g>
+    );
+  }
+
+  return (
+    <ResponsiveContainer width="80%" height="80%" minWidth="32rem" minHeight="20rem">
+      <BarChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+        <Bar yAxisId="left" type="monotone" dataKey="vote_percentage" fill="#56569b" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="key" interval="preserveStartEnd" tick={renderCustomAxisTick} />
+        <YAxis yAxisId="left" unit="%" />
+        <Tooltip />
+        <Legend verticalAlign="top" height={36}/>
+      </BarChart>
+    </ResponsiveContainer>
   )
 }
