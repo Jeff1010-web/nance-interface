abda7cf4228eebd5f95807bb589569df36a56a28
jeff1010-web <wangping51030@gmail.com>
2022-07-18 17:26:39 +0800

    fix: correct class to className

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,13 @@ export default function Home() {
 function HomeCard({ link, linkText, imgSrc, bgColor }) {
   return (
       <Link href={link}>
-        <div class="mt-3 sm:mt-0">
-          <div class="nounish_button" id="one">
-            <button class="group block text-nouns rounded-xl sm:cursor-pointer transition duration-200 xs:h-40 xs:w-40 sm:h-48 sm:w-48 bg-gray-100 focus-within:ring-2 relative focus-within:ring-offset-2 focus-within:ring-offset-gray-100 hover:ring-2 hover:ring-grey-base focus-within:ring-grey-base overflow-hidden m-auto">
-              <div class={`absolute bottom-0 w-full px-4 py-1 justify-center text-lg flex items-end ${bgColor} font-light text-white shadow-lg`}>
+        <div className="mt-3 sm:mt-0">
+          <div className="nounish_button" id="one">
+            <button className="group block text-nouns rounded-xl sm:cursor-pointer transition duration-200 xs:h-40 xs:w-40 sm:h-48 sm:w-48 bg-gray-100 focus-within:ring-2 relative focus-within:ring-offset-2 focus-within:ring-offset-gray-100 hover:ring-2 hover:ring-grey-base focus-within:ring-grey-base overflow-hidden m-auto">
+              <div className={`absolute bottom-0 w-full px-4 py-1 justify-center text-lg flex items-end ${bgColor} font-light text-white shadow-lg`}>
                 {linkText}
               </div>
-              <img class="object-cover w-full h-full" src={imgSrc} alt={linkText} />
+              <img className="object-cover w-full h-full" src={imgSrc} alt={linkText} />
             </button>
           </div>
         </div>
