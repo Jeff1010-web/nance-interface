5611f1325a06353fc16108ec1f2984bc53d29c64
jeff1010-web <wangping51030@gmail.com>
2022-08-18 16:06:54 +0800

    feat: integrate RainbowKit

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 import Head from 'next/head'
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const user = {
     name: 'Tom Cook',
@@ -18,11 +19,6 @@ const navigation = [
     { name: 'Timeline', href: '/history', current: false },
     { name: 'Snapshot', href: '/snapshot/jbdao.eth', current: false },
     { name: 'Juicebox', href: '/juicebox', current: false },
-]
-const userNavigation = [
-    { name: 'Your Profile', href: '#' },
-    { name: 'Settings', href: '#' },
-    { name: 'Sign out', href: '#' },
 ]
 
 function classNames(...classes) {
@@ -79,50 +75,7 @@ export default function SiteNav({ pageTitle, currentIndex }) {
                                         </div>
                                     </div>
                                     <div className="hidden sm:ml-6 sm:flex sm:items-center">
-                                        <button
-                                            type="button"
-                                            className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                        >
-                                            <span className="sr-only">View notifications</span>
-                                            <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                        </button>
-
-                                        {/* Profile dropdown */}
-                                        <Menu as="div" className="ml-3 relative">
-                                            <div>
-                                                <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                    <span className="sr-only">Open user menu</span>
-                                                    <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
-                                                </Menu.Button>
-                                            </div>
-                                            <Transition
-                                                as={Fragment}
-                                                enter="transition ease-out duration-200"
-                                                enterFrom="transform opacity-0 scale-95"
-                                                enterTo="transform opacity-100 scale-100"
-                                                leave="transition ease-in duration-75"
-                                                leaveFrom="transform opacity-100 scale-100"
-                                                leaveTo="transform opacity-0 scale-95"
-                                            >
-                                                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                                    {userNavigation.map((item) => (
-                                                        <Menu.Item key={item.name}>
-                                                            {({ active }) => (
-                                                                <a
-                                                                    href={item.href}
-                                                                    className={classNames(
-                                                                        active ? 'bg-gray-100' : '',
-                                                                        'block px-4 py-2 text-sm text-gray-700'
-                                                                    )}
-                                                                >
-                                                                    {item.name}
-                                                                </a>
-                                                            )}
-                                                        </Menu.Item>
-                                                    ))}
-                                                </Menu.Items>
-                                            </Transition>
-                                        </Menu>
+                                        <ConnectButton />
                                     </div>
                                     <div className="-mr-2 flex items-center sm:hidden">
                                         {/* Mobile menu button */}
@@ -158,34 +111,7 @@ export default function SiteNav({ pageTitle, currentIndex }) {
                                     ))}
                                 </div>
                                 <div className="pt-4 pb-3 border-t border-gray-200">
-                                    <div className="flex items-center px-4">
-                                        <div className="flex-shrink-0">
-                                            <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
-                                        </div>
-                                        <div className="ml-3">
-                                            <div className="text-base font-medium text-gray-800">{user.name}</div>
-                                            <div className="text-sm font-medium text-gray-500">{user.email}</div>
-                                        </div>
-                                        <button
-                                            type="button"
-                                            className="ml-auto bg-white flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                                        >
-                                            <span className="sr-only">View notifications</span>
-                                            <BellIcon className="h-6 w-6" aria-hidden="true" />
-                                        </button>
-                                    </div>
-                                    <div className="mt-3 space-y-1">
-                                        {userNavigation.map((item) => (
-                                            <Disclosure.Button
-                                                key={item.name}
-                                                as="a"
-                                                href={item.href}
-                                                className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
-                                            >
-                                                {item.name}
-                                            </Disclosure.Button>
-                                        ))}
-                                    </div>
+                                    <ConnectButton />
                                 </div>
                             </Disclosure.Panel>
                         </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,47 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
 import { GraphQLClient, ClientContext } from 'graphql-hooks'
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
 import {
   WagmiConfig, createClient,
   configureChains, chain
 } from 'wagmi'
 import { publicProvider } from 'wagmi/providers/public'
 
-const { provider, webSocketProvider } = configureChains(
-  [chain.mainnet, chain.polygon],
+const graphqlClient = new GraphQLClient({
+  url: 'https://hub.snapshot.org/graphql'
+})
+
+// WAGMI and RainbowKit configuration
+const { chains, provider } = configureChains(
+  [chain.mainnet],
   [publicProvider()],
 )
 
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+
 const wagmiClient = createClient({
   autoConnect: true,
-  provider,
-  webSocketProvider,
-})
-
-const graphqlClient = new GraphQLClient({
-  url: 'https://hub.snapshot.org/graphql'
+  connectors,
+  provider
 })
 
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <ClientContext.Provider value={graphqlClient}>
-        <Component {...pageProps} />
-      </ClientContext.Provider>
+      <RainbowKitProvider chains={chains}>
+        <ClientContext.Provider value={graphqlClient}>
+          <Component {...pageProps} />
+        </ClientContext.Provider>
+      </RainbowKitProvider>
     </WagmiConfig>
   )
 }
