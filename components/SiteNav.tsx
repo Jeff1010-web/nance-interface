c3e11c23ae6fa33ccd232bdd5f8a613a3af808cf
jeff1010-web <wangping51030@gmail.com>
2023-01-29 11:29:28 +0800

    improvement: add link to analytic site

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -19,6 +19,7 @@ export default function SiteNav({ pageTitle, description, image, withWallet }: S
         { name: 'Home', href: '/' },
         { name: 'Governance Process', href: 'https://info.juicebox.money/dao/process/' },
         { name: 'How to Submit a Proposal', href: 'https://info.juicebox.money/dao/proposals/' },
+        { name: 'Analytics', href: 'https://app.flipsidecrypto.com/dashboard/snapshot-plus-data-ueqrnb' },
         { name: 'Discord', href: 'https://discord.gg/juicebox' }
     ]
 
@@ -73,6 +74,8 @@ export default function SiteNav({ pageTitle, description, image, withWallet }: S
                                                 <a
                                                     key={item.name}
                                                     href={item.href}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
                                                     className='border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
                                                 >
                                                     {item.name}
@@ -108,6 +111,8 @@ export default function SiteNav({ pageTitle, description, image, withWallet }: S
                                             key={item.name}
                                             as="a"
                                             href={item.href}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
                                             className='border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
                                         >
                                             {item.name}
