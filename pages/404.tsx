20447265dfe0461f953ad7c96ce9701aac3dba3f
jeff1010-web <wangping51030@gmail.com>
2023-03-07 17:56:54 +0800

    improvement: support errMsg parameter in custom error page; use http.cat image to indicate error status code

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Custom404() {
+export default function Custom404({errMsg} : {errMsg?: string}) {
     return (
         <section className="bg-grey-lightest dark:bg-gray-900">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center">
-                    <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
-                    <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something&apos;s missing.</p>
-                    <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can&apos;t find that page. You&apos;ll find lots to explore on the home page. </p>
-                    <div className="inline-flex text-black bg-amber-200 hover:bg-amber-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+                    <Image src="https://http.cat/404" alt="404" width={487} height={450} />
+                    <p className="my-4 text-lg font-light text-gray-500 dark:text-gray-400">{errMsg || "Sorry, we can't find that page. You'll find lots to explore on the home page."}</p>
+                    <div className="inline-flex text-black bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
                         <Link href="/">
                             <a>Back to home</a>
                         </Link>
+                    </div>
+                    <div className="inline-flex text-black bg-amber-200 hover:bg-amber-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4 ml-4">
+                        <a href="https://discord.com/channels/775859454780244028/955977240787685416" target="_blank" rel="noopener noreferrer">Contact us</a>
                     </div>
                 </div>
             </div>
