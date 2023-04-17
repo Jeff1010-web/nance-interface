eb337afe54a59aa0d96a39fb7b8895c04b81d27f
jeff1010-web <wangping51030@gmail.com>
2023-04-17 21:01:10 +0800

    style: fix version inconsistency of tailwindcss which has changed the specificity rule

--- components/GenericButton.tsx
+++ components/GenericButton.tsx
@@ -4,7 +4,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
 }
 
-const DEFAULT_STYLE = "w-fit m-2 inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-2 py-1 text-sm font-medium disabled:text-black text-white shadow-sm hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-300"
+const DEFAULT_STYLE = "w-fit inline-flex items-center justify-center rounded-md border border-transparent bg-blue-600 px-2 py-1 text-sm font-medium disabled:text-black text-white shadow-sm hover:bg-blue-500 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-300"
 
 export default function GenericButton({ children = null, className = "", onClick = undefined }) {
   return (
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -233,7 +233,7 @@ function Form({ space }: { space: string }) {
 
           {!jrpcSigner && (
             <button onClick={() => openConnectModal()}
-              className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-gray-400"
+              className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-blue-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:bg-gray-400"
             >
               Connect Wallet
             </button>
