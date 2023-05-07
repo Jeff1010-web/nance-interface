8c6bbd96a8211ae7847ff2d947d1f98c35fa69b2
jeff1010-web <wangping51030@gmail.com>
2023-05-07 12:32:52 -0400

    feature: nance coauthors

--- pages/p/[proposal].tsx
+++ pages/p/[proposal].tsx
@@ -5,7 +5,7 @@ import ReactMarkdown from "react-markdown";
 import { Tooltip } from 'flowbite-react';
 import FormattedAddress from "../../components/FormattedAddress";
 import { format, toDate } from "date-fns";
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, Fragment } from "react";
 import VotingModal from "../../components/VotingModal";
 import { withDefault, NumberParam, createEnumParam, useQueryParams, StringParam } from "next-query-params";
 import { processChoices } from "../../libs/snapshotUtil";
@@ -96,6 +96,7 @@ interface ProposalCommonProps {
   status: string;
   title: string;
   author: string;
+  coauthors: string[];
   body: string;
   created: number;
   end: number;
@@ -133,6 +134,7 @@ export default function NanceProposalPage({ proposal, snapshotProposal }: { prop
     status: snapshotProposal?.state || proposal.status,
     title: snapshotProposal?.title || proposal.title,
     author: snapshotProposal?.author || proposal.authorAddress,
+    coauthors: proposal?.coauthors || [],
     body: snapshotProposal?.body || proposal.body,
     created: snapshotProposal?.start || Math.floor(new Date(proposal.date).getTime() / 1000),
     end: snapshotProposal?.end || 0,
@@ -282,6 +284,17 @@ function ProposalContent({ body }: { body: string }) {
           by&nbsp;
           <FormattedAddress address={commonProps.author} style="text-gray-500" overrideURLPrefix="/snapshot/profile/" openInNewWindow={false} />
         </p>
+        { commonProps.coauthors.length > 0 && (
+          <p className="text-sm text-gray-500 text-right">
+            co-authored by&nbsp;
+            {commonProps.coauthors.map((coauthor, i) => (
+              <Fragment key={i}>
+                <FormattedAddress address={coauthor} style="text-gray-500" overrideURLPrefix="/snapshot/profile/" openInNewWindow={false} />
+                {i < commonProps.coauthors.length - 1 && ', '}
+              </Fragment>
+            ))}
+          </p>
+        )}
 
         {/* Metadata */}
         <div className="my-4 border bg-gray-100 rounded-md px-4 py-5 sm:px-6">
