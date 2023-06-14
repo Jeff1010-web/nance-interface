afb8abe7426833e617e65115307cc8eb47a83387
jeff1010-web <wangping51030@gmail.com>
2023-06-14 19:18:57 +0800

    fix: use getServerSideProps to render correct metadata for OpenGraph

--- pages/s/[space]/index.tsx
+++ pages/s/[space]/index.tsx
@@ -3,9 +3,18 @@ import { useRouter } from "next/router"
 import Footer from "../../../components/Footer"
 import NanceSpace from "../../../components/nance/Space";
 
-export default function NanceSpacePage() {
-  const router = useRouter();
-  let space = router.query.space as string;
+export async function getServerSideProps(context) {
+  const spaceParam: string = context.params.space;
+
+  // Pass data to the page via props
+  return {
+    props: {
+      space: spaceParam
+    }
+  }
+}
+
+export default function NanceSpacePage({ space } : { space: string }) {
   return (
     <>
       <SiteNav pageTitle={`${space} Governance`} description={`${space} Governance Platform`} image="/images/opengraph/homepage.png" space={space} withWallet />
