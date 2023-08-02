d804e55abee1cb9bc2736f158d1bebc23ba9fbb6
jeff1010-web <wangping51030@gmail.com>
2023-08-02 10:30:35 -0400

    bug: . ?

--- pages/s/[space]/index.tsx
+++ pages/s/[space]/index.tsx
@@ -2,6 +2,7 @@ import SiteNav from "../../../components/SiteNav";
 import Footer from "../../../components/Footer";
 import NanceSpace from "../../../components/nance/Space";
 import { useSpaceInfo } from "../../../hooks/NanceHooks";
+import { useRouter } from "next/router";
 
 export async function getServerSideProps(context: any) {
   const spaceParam: string = context.params.space;
@@ -15,8 +16,9 @@ export async function getServerSideProps(context: any) {
 }
 
 export default function NanceSpacePage({ space } : { space: string }) {
-  const { data } = useSpaceInfo({ space });
-  const spaceImage = data?.data.name === 'juicebox' ? '/images/opengraph/homepage.png' : `https://cdn.stamp.fyi/space/${data?.data.snapshotSpace}?s=500`;
+  const router = useRouter();
+  const { data } = useSpaceInfo({ space }, router.isReady);
+  const spaceImage = data?.data.name === 'juicebox' ? '/images/opengraph/homepage.png' : `https://cdn.stamp.fyi/space/${data?.data?.snapshotSpace}?s=500`;
   return (
     <>
       <SiteNav pageTitle={`${space} Governance`} description={`${space} Governance Platform`} image={spaceImage} space={space} withWallet withProposalButton={false} />
