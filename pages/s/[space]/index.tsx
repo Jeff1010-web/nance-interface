db85b2a82e981754166bf5641c5dd9f7752bbfbe
jeff1010-web <wangping51030@gmail.com>
2023-08-02 11:27:51 -0400

    bug: didnt work trying with serverside props

--- pages/s/[space]/index.tsx
+++ pages/s/[space]/index.tsx
@@ -1,29 +1,22 @@
 import SiteNav from "../../../components/SiteNav";
 import Footer from "../../../components/Footer";
 import NanceSpace from "../../../components/nance/Space";
-import { useSpaceInfo } from "../../../hooks/NanceHooks";
+import { NANCE_API_URL } from "../../../constants/Nance";
 
 export async function getServerSideProps(context: any) {
   const spaceParam: string = context.params.space;
-
+  const { data } = await fetch(`${NANCE_API_URL}/${spaceParam}`).then((res) => res.json());
   // Pass data to the page via props
   return {
     props: {
-      space: spaceParam
+      space: spaceParam,
+      snapshotSpace: data.snapshotSpace,
     }
   };
 }
 
-export default function NanceSpacePage({ space } : { space: string }) {
-  const { data, isLoading } = useSpaceInfo({ space });
-  const spaceImage = data?.data?.name === 'juicebox' ? '/images/opengraph/homepage.png' : `https://cdn.stamp.fyi/space/${data?.data?.snapshotSpace}?s=500`;
-  if (isLoading) {
-    return (
-      <div className="flex justify-center pt-20">
-        <div className="animate-spin inline-block w-20 h-20 border-[5px] border-current border-t-transparent text-gray-900 rounded-full mr-2" role="status" aria-label="loading" />
-      </div>
-    );
-  }
+export default function NanceSpacePage({ space, snapshotSpace } : { space: string, snapshotSpace: string }) {
+  const spaceImage = (space === 'juicebox') ? '/images/opengraph/homepage.png' : `https://cdn.stamp.fyi/space/${snapshotSpace}?s=500`;
   return (
     <>
       <SiteNav pageTitle={`${space} Governance`} description={`${space} Governance Platform`} image={spaceImage} space={space} withWallet withProposalButton={false} />
