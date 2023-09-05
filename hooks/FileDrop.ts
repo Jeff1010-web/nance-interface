fd31d8153bcac2b6c943a673fe4064e77704c84c
jeff1010-web <wangping51030@gmail.com>
2023-09-05 11:58:18 +0800

    build: use yarnrc

--- hooks/FileDrop.ts
+++ hooks/FileDrop.ts
@@ -2,10 +2,10 @@ import { markdownToHtml } from "../libs/markdown";
 import { Editor } from "tinymce";
 
 export const fileDrop = (editor: Editor) => {
-  editor.on('drop', async function (e) {
+  editor.on('drop', async function(e) {
     e.preventDefault(); // prevent default drop behavior
     const file = e.dataTransfer?.items[0].getAsFile();
-    if (file?.name.endsWith('.md')) {      
+    if (file?.name.endsWith('.md')) {
       const md = await file.text();
       const html = await markdownToHtml(md);
       editor.setContent(html + '\n\n' + editor.getContent());
