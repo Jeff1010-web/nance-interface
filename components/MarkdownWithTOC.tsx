77bc7af49a3f6e20423f3fee369af32e23fa24d2
jeff1010-web <wangping51030@gmail.com>
2023-09-07 08:43:12 +0800

    fix(MarkdownWithTOC.tsx): add section header between TOC and next section so the text in between won't be replaced by auto-generated TOC
    https://github.com/remarkjs/remark-toc/issues/18

--- components/MarkdownWithTOC.tsx
+++ components/MarkdownWithTOC.tsx
@@ -6,13 +6,14 @@ import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import ReactMarkdown from 'react-markdown';
 import React from 'react';
-import {h} from 'hastscript';
+import { h } from 'hastscript';
 
 export default function MarkdownWithTOC({ body }: { body: string }) {
   return (
     <article className="prose prose-lg prose-indigo mx-auto text-gray-500 break-words">
-      <ReactMarkdown remarkPlugins={[remarkGfm, remarkToc]} 
-        rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeSlug, 
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm, remarkToc]}
+        rehypePlugins={[rehypeRaw, rehypeSanitize, rehypeSlug,
           [rehypeAutolinkHeadings, {
             content(node: any) {
               return [
@@ -22,13 +23,13 @@ export default function MarkdownWithTOC({ body }: { body: string }) {
             behavior: 'append'
           }]]}
         components={{
-          h2: ({node, ...props}) => <h2 className="group" {...props} />,
-          h3: ({node, ...props}) => <h3 className="group" {...props} />,
-          h4: ({node, ...props}) => <h4 className="group" {...props} />,
-          h5: ({node, ...props}) => <h5 className="group" {...props} />,
-          h6: ({node, ...props}) => <h6 className="group" {...props} />
+          h2: ({ node, ...props }) => <h2 className="group" {...props} />,
+          h3: ({ node, ...props }) => <h3 className="group" {...props} />,
+          h4: ({ node, ...props }) => <h4 className="group" {...props} />,
+          h5: ({ node, ...props }) => <h5 className="group" {...props} />,
+          h6: ({ node, ...props }) => <h6 className="group" {...props} />
         }}>
-        {"## Table of contents \n" + body}
+        {"## Table of contents \n\n ## Contents \n\n" + body}
       </ReactMarkdown>
     </article>
   );
