76e25b5cc6863b46ea888f89dfff62b0fcbe158a
jeff1010-web <wangping51030@gmail.com>
2023-09-13 10:36:24 +0800

    style(404.tsx-&-500.tsx): add OpenGraph data

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,25 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
+import SiteNav from "../components/SiteNav";
 
 export default function Custom500({ errMsg }: { errMsg?: string }) {
+  const imageSrc = "https://http.cat/500";
+  const errLabel = errMsg || "Sorry, we can't present that page now due to some errors. You'll find lots to explore on the home page.";
+
   return (
-    <section className="bg-grey-lightest dark:bg-gray-900">
-      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div className="mx-auto max-w-screen-sm text-center">
-          <Image src="https://http.cat/500" alt="500" width={487} height={450} />
-          <p className="my-4 text-lg font-light text-gray-500 dark:text-gray-400">{errMsg || "Sorry, we can't present that page now due to some errors. You'll find lots to explore on the home page."}</p>
-          <div className="inline-block text-black bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4 mr-4">
-            <Link href="/" legacyBehavior>
-              <a>Back to home</a>
-            </Link>
-          </div>
-          <div className="inline-block text-black bg-amber-200 hover:bg-amber-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
-            <Link href="discord://discord.com/channels/1090064637858414633/1090064837498896395" legacyBehavior>
-              <a>Contact us</a>
-            </Link>
+    <>
+      <SiteNav pageTitle="Internal Server Error"
+        description={errLabel}
+        image={imageSrc} />
+
+      <section className="bg-grey-lightest dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div className="mx-auto max-w-screen-sm text-center">
+            <Image src={imageSrc} alt="500" width={487} height={450} />
+            <p className="my-4 text-lg font-light text-gray-500 dark:text-gray-400">{errLabel}</p>
+            <div className="inline-block text-black bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4 mr-4">
+              <Link href="/" legacyBehavior>
+                <a>Back to home</a>
+              </Link>
+            </div>
+            <div className="inline-block text-black bg-amber-200 hover:bg-amber-300 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+              <Link href="discord://discord.com/channels/1090064637858414633/1090064837498896395" legacyBehavior>
+                <a>Contact us</a>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
