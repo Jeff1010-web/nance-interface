ecff7e49ab41c7db0133e230d1fe1c3b81c9deb2
jeff1010-web <wangping51030@gmail.com>
2023-11-08 16:16:26 +0800

    build: integrate storybook-addon-module-mock which can mock modules

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,6 +11,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "storybook-addon-module-mock",
   ],
   framework: {
     name: "@storybook/nextjs",
