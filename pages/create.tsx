6fa04751c01c64f635e5368eff8c6caad2bbca53
jeff1010-web <wangping51030@gmail.com>
2023-12-20 11:46:20 +0800

    feat: add marginTop to backnextbutton

--- pages/create.tsx
+++ pages/create.tsx
@@ -249,9 +249,9 @@ function Form() {
 }
 
 const BackNextButtons = ({ back, next }: { back?: () => void, next?: () => void}) => (
-  <div className="flex justify-end space-x-6">
+  <div className="flex justify-end space-x-6 mt-4">
     {back && <button
-      className="inline-flex w-fit items-center justify-center rounded-md border border-transparent bg-gray-400 px-4 py-2 text-sm text-white shadow-sm hover:bg-gray-500" 
+      className="inline-flex w-fit items-center justify-center rounded-md border border-transparent bg-gray-400 px-4 py-2 text-sm text-white shadow-sm hover:bg-gray-500"
       onClick={back}>Back</button>}
     {next && <button
       className="inline-flex w-fit items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm text-white shadow-sm hover:bg-indigo-500"
