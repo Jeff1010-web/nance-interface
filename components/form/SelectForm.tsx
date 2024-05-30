b9983affbbdb17fc4c34862119e370bc3dfc8fbb
jeff1010-web <wangping51030@gmail.com>
2024-05-30 18:08:21 +0800

    feat: add showType param to input components

--- components/form/SelectForm.tsx
+++ components/form/SelectForm.tsx
@@ -1,32 +1,56 @@
+import { classNames } from "@/utils/functions/tailwind";
 import { ErrorMessage } from "@hookform/error-message";
 import { useFormContext } from "react-hook-form";
 
 export interface SelectOption {
-    displayValue: string,
-    value: string
+  displayValue: string;
+  value: string;
 }
 
-export default function SelectForm(
-  { label, fieldName, options, fieldType = "string", defaultValue = "" } : { label: string, fieldName: any, options: SelectOption[], fieldType?: string, defaultValue?: string }
-) {
-  const { register, formState: { errors } } = useFormContext();
+export default function SelectForm({
+  label,
+  fieldName,
+  options,
+  fieldType = "string",
+  defaultValue = "",
+  showType = true,
+}: {
+  label: string;
+  fieldName: any;
+  options: SelectOption[];
+  fieldType?: string;
+  defaultValue?: string;
+  showType?: boolean;
+}) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
 
   return (
     <div>
-      <label className="block text-sm font-medium text-gray-700">
-        {label}
-      </label>
+      <label className="block text-sm font-medium text-gray-700">{label}</label>
       <div className="mt-1 flex rounded-md shadow-sm">
-        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-          {fieldType}
-        </span>
+        {showType && (
+          <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+            {fieldType}
+          </span>
+        )}
+
         <select
-          {...register(fieldName,
-            { shouldUnregister: true, value: defaultValue })}
-          className="block w-full rounded-none rounded-r-md border border-gray-300 bg-white h-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+          {...register(fieldName, {
+            shouldUnregister: true,
+            value: defaultValue,
+          })}
+          className={classNames(
+            "block w-full border border-gray-300 bg-white h-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm",
+            showType ? "rounded-none rounded-r-md" : "rounded-md",
+          )}
         >
           {options.map((o, i) => (
-            <option key={i} value={o.value}>{o.displayValue}</option>
+            <option key={i} value={o.value}>
+              {o.displayValue}
+            </option>
           ))}
         </select>
       </div>
--- components/form/UIntForm.tsx
+++ components/form/UIntForm.tsx
@@ -1,6 +1,7 @@
 import { ErrorMessage } from "@hookform/error-message";
 import { useFormContext } from "react-hook-form";
 import GenericButton from "../common/GenericButton";
+import { classNames } from "@/utils/functions/tailwind";
 
 /**
  * UIntForm is a form component for uint input with a button to add decimal zeros.
@@ -11,12 +12,14 @@ export default function UIntForm({
   fieldType = "uint256",
   decimal = 18,
   defaultValue = 0,
+  showType = true,
 }: {
   label: string;
   fieldName: any;
   fieldType?: string;
   decimal?: number;
   defaultValue?: number;
+  showType?: boolean;
 }) {
   const {
     register,
@@ -29,9 +32,12 @@ export default function UIntForm({
     <div>
       <label className="block text-sm font-medium text-gray-700">{label}</label>
       <div className="mt-1 flex rounded-md shadow-sm">
-        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
-          {fieldType}
-        </span>
+        {showType && (
+          <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-sm text-gray-500">
+            {fieldType}
+          </span>
+        )}
+
         <input
           type="number"
           step={1}
@@ -41,7 +47,10 @@ export default function UIntForm({
             shouldUnregister: true,
             required: "Can't be empty",
           })}
-          className="block h-10 w-full flex-1 rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          className={classNames(
+            "block h-10 w-full flex-1 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm",
+            showType ? "rounded-none rounded-r-md" : "rounded-md",
+          )}
         />
       </div>
       <ErrorMessage
